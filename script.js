36f0cf67542a9dc1e2f6dcc988eadd10be8384cc
Sikriwal9570 <128585704+Sikriwal9570@users.noreply.github.com>
2023-10-01 16:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
+function numberOfBooksRead(library) {
+  // Initialize a variable to keep track of the count of read books
+  let count = 0;
+
+  // Loop through the books in the library
+  for (let i = 0; i < library.length; i++) {
+    // Check if the readingStatus of the current book is true (read)
+    if (library[i].readingStatus === true) {
+      // If it is, increment the count
+      count++;
+    }
+  }
+
+  // Return the count of read books
+  return count;
+}
+
+// Example usage:
 const library = [
   {
-    author: "Bill Gates",
-    title: "The Road Ahead",
+    author: "Author 1",
+    title: "Book 1",
     readingStatus: true,
   },
   {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
+    author: "Author 2",
+    title: "Book 2",
     readingStatus: false,
+  },
+  {
+    author: "Author 3",
+    title: "Book 3",
+    readingStatus: true,
   },
 ];
 
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
-// Do not change the code below
-
-alert(numberOfBooksRead());
+const numberOfReadBooks = numberOfBooksRead(library);
+console.log("Number of read books:", numberOfReadBooks);
